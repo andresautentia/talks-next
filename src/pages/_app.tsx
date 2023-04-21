63d1fe43d7345d64ccaaeac9a818d51e90b7b63c
Andrés <andres.fuentes@autentia.com>
2023-04-21 09:45:50 +0200

    feat: adds font-family with nextjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,23 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import "reflect-metadata";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import 'reflect-metadata'
+
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+    weight: ['100', '400', '500', '700'],
+    subsets: ['latin'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return (
+        <>
+            <style jsx global>{`
+                html {
+                    font-family: ${roboto.style.fontFamily};
+                }
+            `}</style>
+            <Component {...pageProps} />
+        </>
+    )
 }
