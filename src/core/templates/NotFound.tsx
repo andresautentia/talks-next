60fd2f6d57377194d49c9f7a0c0157ce80d594a7
Andrés <andres.fuentes@autentia.com>
2023-04-21 08:35:22 +0200

    fix: image error in 404

--- src/core/templates/NotFound.tsx
+++ src/core/templates/NotFound.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import image404 from '../assets/404.jpg'
+import Image from 'next/image'
 
 export const NotFound: React.FC = () => {
     return (
         <div className="not-found">
-            <img src={image404} alt="Página no encontrada" />
+            <Image src={image404} alt="Página no encontrada" />
         </div>
     )
 }
